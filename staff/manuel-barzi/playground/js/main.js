9ec78f57af5e7bba11d4749bbae3b0bd7af3216d
Manuel Barzi <manuelbarzi@gmail.com>
2024-10-15 21:18:55 +0200

    add js demo #1'

--- staff/manuel-barzi/playground/js/main.js
+++ staff/manuel-barzi/playground/js/main.js
@@ -49,21 +49,111 @@ d = function (name) { return 'Hello, ' + name + '!' }
 //console.log(d('Wendy'))
 //console.log(d())
 
-var a = []
+var a = [] // new Array
 a['0'] = 100
 a[1] = 200
 a[5] = 600
-console.dir(a)
+//console.dir(a)
 
-var b = { length: 0 }
+var b = { length: 0 } // new Object
 b[0] = 100
 b[1] = 200
 b[5] = 600
 b['length'] = 6
-console.dir(b)
+//console.dir(b)
 
+/*
 for (var i = 0; i < b.length; i++) {
     var elem = b[i]
 
     console.log(elem)
 }
+*/
+
+window.name = 'Window'
+window.image = '🪟'
+
+var peter = { name: 'Peter', image: '🥶' }
+var wendy = { name: 'Wendy', image: '🥵' }
+
+function hello() {
+    return this.image + ' ' + this.name + ': Hello!'
+}
+
+/*
+console.log(window.hello())
+console.log(hello())
+console.log(hello.call(peter))
+console.log(hello.call(wendy))
+*/
+
+wendy.hi = hello
+//console.log(wendy.hi === hello)
+console.log(wendy.hi())
+
+peter.yo = hello
+console.log(peter.yo())
+
+a.hello = hello
+a.name = 'Array'
+a.image = '📦'
+console.log(a.hello())
+
+a[10] = peter
+console.log(a.length)
+
+b[10] = wendy
+console.log(b.length)
+b.length = 11
+
+peter.partner = wendy
+wendy.partner = peter
+
+console.log(peter.yo())
+console.log(a[10].yo())
+console.log(b[10].partner.yo())
+console.log(b[10].partner.partner.partner.yo())
+
+var oswald = {
+    name: 'Oswald',
+    getPeter: function () { return peter }
+}
+
+b[11] = oswald
+b.length = 12
+
+a[3] = b
+
+console.log(a[3][10].partner.yo())
+console.log(a[3][11].getPeter().yo())
+
+var f = function a() {
+    return function b() {
+        return [, , function c() {
+            return {
+                o: {
+                    hi: function () { return 'hi!' }
+                }
+            }
+        }]
+    }
+}
+
+console.log(f()()[2]().o.hi())
+
+var g = ['hola', 1, true, {
+},
+    {
+        0: [, , , , {
+            b: function () {
+                return {
+                    c: function () {
+                        return [function () { return 'yo' }]
+                    }
+                }
+            }
+        }],
+        length: 1
+    }]
+
+console.log(g[4][0][4].b().c()[0]())
