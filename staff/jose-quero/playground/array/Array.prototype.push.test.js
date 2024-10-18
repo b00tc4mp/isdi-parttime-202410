7c8281b32fe18f936de503a9f4576369336b8b08
Jose Quero <jose_jarote1605@hotmail.com>
2024-10-18 11:14:11 +0200

    add dorraymon - array - at test #6

---
+++ staff/jose-quero/playground/array/Array.prototype.push.test.js
@@ -0,0 +1,37 @@
+console.log('TEST Array.prototype.push')
+
+console.log('CASE add character to sponge bob characters')
+
+var chars = new Array
+chars[0] = 'Sponge Bob'
+chars[1] = 'Patrick Star'
+chars[2] = 'Squidward Tentacles'
+chars[3] = 'Gary Snail'
+var length = chars.push('Sheldon Plankton')
+console.log(length)
+//5
+console.log(chars)
+//['Sponge Bob', 'Patrick Star', 'Squidward Tentacles',  'Gary Snail',  'Sheldon Plankton']
+
+console.log('CASE add various html tags')
+
+var tags = new Array
+tags[0] = 'html'
+tags[1] = 'head'
+tags[2] = 'body'
+tags.push('h1', 'p', 'table', 'img', 'video')
+console.log(length)
+// 8
+console.log(tags)
+// ['html',  'head',  'body',  'h1',  'p',  'table',  'img',  'video',]
+
+console.log('CASE add various arrays into nums')
+
+var nums = new Array
+nums.push([1, 2, 3], [4, 5], [6], [], [7], [8, 9, 10, 11])
+console.log(length)
+// 6
+console.log(nums)
+//([1, 2, 3], [4, 5], [6], [], [7], [8, 9, 10, 11])
+console.log(nums[0][2])
+// 3
---
+++ staff/jose-quero/playground/array/dorraymon/Dorraymon.prototype.push.test.js
@@ -0,0 +1,26 @@
+function Dorraymon() {
+    this.length = 0
+}
+
+Dorraymon.prototype.push = function (element) {
+    this[this.length] = element
+    this.length++ // this.length = this.length + 1 // this.length += 1
+
+    return this.length
+}
+
+console.log('TEST Dorraymon.prototype.push')
+
+console.log('CASE add character to sponge bob characters')
+
+var chars = new Dorraymon
+chars[0] = 'Sponge Bob'
+chars[1] = 'Patrick Star'
+chars[2] = 'Squidward Tentacles'
+chars[3] = 'Gary Snail'
+chars.length = 4
+var length = chars.push('Sheldon Plankton')
+console.log(length)
+// 5
+console.log(chars)
+// Dorraymon { 0: 'Sponge Bob', 1: 'Patrick Star', 2; 'Squidward Tentacles', 3; 'Gary Snail', 4; 'Sheldon Plankton', length 5  }
